937f57e9336e6b283fd4c189ea29a3ba6204af14
beshmabher <bishamberc@gmail.com>
2021-09-12 20:16:07 +0530

    changed sql migration to use postgresql

--- src/main/resources/db/migration/V1__initial_data.sql
+++ src/main/resources/db/migration/V1__initial_data.sql
@@ -1,5 +1,5 @@
-create table role (id varchar(36) not null, created_at datetime, is_editable boolean default true, name varchar(50) not null, updated_at datetime, primary key (id));
-create table user (username varchar(100) not null, avatar varchar(255), created_at datetime, email varchar(100) not null, first_name varchar(20) not null, last_name varchar(30), location varchar(255), updated_at datetime, role_id varchar(36), primary key (username));
+create table role (id varchar(36) not null, created_at timestamp, is_editable boolean default true, name varchar(50) not null, updated_at timestamp, primary key (id));
+create table user (username varchar(100) not null, avatar varchar(255), created_at timestamp, email varchar(100) not null, first_name varchar(20) not null, last_name varchar(30), location varchar(255), updated_at timestamp, role_id varchar(36), primary key (username));
 
 alter table user add constraint UK_ob8kqyqqgmefl0aco34akdtpe unique (email);
 alter table user add constraint FKn82ha3ccdebhokx3a8fgdqeyy foreign key (role_id) references role (id);
